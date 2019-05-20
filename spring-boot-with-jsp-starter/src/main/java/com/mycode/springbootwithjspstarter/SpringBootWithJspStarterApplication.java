1f35f027f0dc2a90b19d6aa7f8dac95147b6f010
Sandeep Amarnath <sandeepamarnath@Sandeeps-MacBook-Pro.local>
2019-05-19 23:35:31 -0400

    first commit

---
+++ spring-boot-with-jsp-starter/src/main/java/com/mycode/springbootwithjspstarter/SpringBootWithJspStarterApplication.java
@@ -0,0 +1,13 @@
+package com.mycode.springbootwithjspstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWithJspStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWithJspStarterApplication.class, args);
+	}
+
+}
---
+++ spring-boot-with-jsp-starter/src/main/webapp/WEB-INF/jsp/welcome.jsp
@@ -0,0 +1,8 @@
+Hello Sandeep Welcome !
+
+
+<hr>
+This is your JSP, Spring boot starter project. 
+
+<hr>
+You can use this to start building your JSP, Spring boot apps
